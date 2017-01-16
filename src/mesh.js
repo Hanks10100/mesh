38912c9821452492f4bf90bfae84bded737afcf4
Hanks <zhanghan.me@gmail.com>
2017-01-16 22:37:28 +0800

    samplify the code

--- src/mesh.js
+++ src/mesh.js
@@ -1,50 +1,58 @@
 import { getMeshStyle } from './parser'
 
-function getChildren (children) {
+// filter empty vnode
+function filterChildren (children) {
   if (Array.isArray(children)) {
     return children.filter(vnode => !!vnode.tag)
   }
   return []
 }
 
+function extend (to, _from) {
+  for (const key in _from) {
+    to[key] = _from[key]
+  }
+  return to
+}
+
 function install (Vue) {
   Vue.component('mesh', {
     name: 'mesh',
     props: {
-      width: {
-        type: Number,
-        default: 750
-      },
-      column: {
-        type: Number,
-        required: true,
-        default: 12
-      },
+      width: [Number, String], // default 750
+      column: [Number, String], // default 12
+      gap: [Number, String], // default 0
       layout: {
         type: [Array, String],
         required: true,
         default: []
-      },
-      gap: {
-        type: Number,
-        default: 0
       }
     },
 
-    render (h, context) {
+    render (createElement) {
       const { wrapperStyle, layoutStyle } = getMeshStyle(this)
-      const children = getChildren(this.$slots.default)
-      return h('div', { staticStyle: wrapperStyle }, children.map((vnode, i) => {
-        vnode.data.staticStyle = layoutStyle[i]
-        const options = vnode.componentOptions
-        if (options.tag === 'mesh') {
-          if (!options.propsData) options.propsData = {}
-          options.propsData.width = parseFloat(layoutStyle[i].width)
-        }
-        return vnode
-      }))
+      return createElement(
+        'div',
+        { staticStyle: wrapperStyle },
+        filterChildren(this.$slots.default).map((vnode, i) => {
+          vnode.data.staticStyle = extend(vnode.data.staticStyle || {}, layoutStyle[i])
+
+          // support nested mesh
+          const options = vnode.componentOptions
+          if (options && options.tag === 'mesh') {
+            if (!options.propsData) options.propsData = {}
+            options.propsData.width = parseFloat(layoutStyle[i].width)
+          }
+          return vnode
+        })
+      )
     }
   })
+}
+
+// auto install in browser
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
 }
 
 export default {
--- src/parser.js
+++ src/parser.js
@@ -1,23 +1,20 @@
 import { computeLayout } from './layout'
 
-function unit (number) {
+export function unit (number) {
   if (Number(number) !== 0 && typeof WXEnvironment === 'object' && WXEnvironment.platform === 'Web') {
     return number + 'px'
   }
-  return number
+  return Number(number)
 }
 
+// parse string layout param
 export function parseLayout (layout) {
-  if (Array.isArray(layout)) {
-    return layout
-  }
   if (typeof layout === 'string') {
     return layout.split(/\s*\|\s*/).map(pair => pair.split(/\s*\,\s*/).map(Number))
   }
-  return []
+  return Array.isArray(layout) ? layout : []
 }
 
-// 根据属性值计算布局样式
 export function getMeshStyle (props) {
   const width = Number(props.width) || 750
   const column = Number(props.column) || 12
@@ -25,28 +22,22 @@ export function getMeshStyle (props) {
   const gap = Number(props.gap) || 0
 
   const ratio = (width + gap) / column
-  const origins = computeLayout({ column, layout })
+  const origins = computeLayout(column, layout)
 
-  // 计算外框样式
-  const wrapperStyle = {
-    position: 'relative',
-    width: unit(width),
-    height: unit(origins.pop().y * ratio - gap)
-  }
-
-  // 计算布局样式
-  const layoutStyle = layout.map((size, index) => {
-    const coord = origins[index]
-    return {
+  return {
+    wrapperStyle: {
+      position: 'relative',
+      width: unit(width),
+      height: unit(origins.pop().y * ratio - gap)
+    },
+    layoutStyle: layout.map((size, i) => ({
       position: 'absolute',
       justifyContent: 'center',
       alignItems: 'center',
-      top: unit(coord.y * ratio),
-      left: unit(coord.x * ratio),
+      top: unit(origins[i].y * ratio),
+      left: unit(origins[i].x * ratio),
       width: unit(size[0] * ratio - gap),
       height: unit(size[1] * ratio - gap)
-    }
-  })
-
-  return { wrapperStyle, layoutStyle }
+    }))
+  }
 }
