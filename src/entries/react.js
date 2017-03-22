b0b3e1ed4026117f59ab78cf27b1c5ee7abaf981
Hanks <zhanghan.me@gmail.com>
2017-03-22 15:20:29 +0800

    create react mesh component

--- src/entries/react.js
+++ src/entries/react.js
@@ -1,5 +1,44 @@
-import { getMeshStyle } from '../parser'
+import React from 'react'
+import { parseOrders, getMeshStyle } from '../parser'
 
-export default {
-  getMeshStyle
+function picker (vnode, attr) {
+  const props = vnode.props
+  if (props && props[attr]) {
+    return props[attr]
+  }
+  return null
+}
+
+export default function ReactMeshComponent (props) {
+  const orders = parseOrders(props, props.children, picker)
+  const { wrapperStyle, layoutStyle } = getMeshStyle(props, orders)
+  return React.createElement(
+    'div',
+    { style: Object.assign({}, props.style, wrapperStyle) },
+    React.Children.map(props.children, (vnode, i) => {
+      const props = {
+        style: Object.assign({}, vnode.props.style, layoutStyle[i])
+      }
+
+      // support nested mesh
+      if (vnode.type === ReactMeshComponent) {
+        props.width = parseFloat(layoutStyle[i].width)
+      }
+
+      return React.cloneElement(vnode, props)
+    })
+  )
+}
+
+const { arrayOf, number, oneOfType, string } = React.PropTypes
+ReactMeshComponent.propTypes = {
+  width: oneOfType([number, string]),
+  column: oneOfType([number, string]),
+  gap: oneOfType([number, string]),
+  orders: oneOfType([arrayOf(number), string]),
+  layout: oneOfType([arrayOf(arrayOf(number)), string]).isRequired
+}
+
+ReactMeshComponent.defaultProps = {
+  layout: []
 }
